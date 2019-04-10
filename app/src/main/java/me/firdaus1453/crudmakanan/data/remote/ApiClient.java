669f4a55ba47573cfbd498c82c944d585468cd87
firdaus1453 <mfirdaus1453@gmail.com>
2019-04-10 22:16:54 +0800

    Add logging interceptor

--- app/src/main/java/me/firdaus1453/crudmakanan/data/remote/ApiClient.java
+++ app/src/main/java/me/firdaus1453/crudmakanan/data/remote/ApiClient.java
@@ -1,6 +1,8 @@
 package me.firdaus1453.crudmakanan.data.remote;
 
 import me.firdaus1453.crudmakanan.utils.Constants;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,9 +14,23 @@ public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(){
+
+        // membuat object logging interceptor
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+        // set log level
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        // Membuat object httpClient
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        // Menambahkan logging interceptor ke dalam httpClient
+        httpClient.addInterceptor(logging);
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
                 .build();
         return retrofit;
     }
